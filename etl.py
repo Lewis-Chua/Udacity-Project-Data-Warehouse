5f88ec5a18c3cdaa186b39290977f12bfbabb015
Lewis Chua <73373002+Lewis-Chua@users.noreply.github.com>
2023-08-27 22:10:58 +0800

    Update etl.py
    Add docstrings for all the functions

--- etl.py
+++ etl.py
@@ -4,18 +4,30 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load data from S3 bucket into staging tables.
+    """
+    
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data from staging tables into final tables. 
+    """
+    
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Reads database configuration from `dwh.cfg`, establishes a connection to the database, executes the load and insert function above, and then closes the connection.
+    """
+    
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
